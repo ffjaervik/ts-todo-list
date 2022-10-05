407a61e9a81331c2bcdcb419083bada36db7f4f1
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-10-05 15:32:17 +0100

    css declared

--- ts-react-todo-app/src/App.tsx
+++ ts-react-todo-app/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-
 import './App.css';
 
-
-
-function App() {
+const App:React.FC = () => {
   return (
     <div className="App">
-     hello world
+     <span className='heading'>
+        Taskify
+      </span>
     </div>
   );
 }
