85183e7f013e51318d300dc855987d97cd4a4f40
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-10-06 11:05:45 +0100

    completed css

--- ts-react-todo-app/src/components/InputField.tsx
+++ ts-react-todo-app/src/components/InputField.tsx
@@ -5,7 +5,7 @@ const InputField = () => {
   return (
     <form className='input'>
         <input type='input' placeholder='Add a task' className='input__box' />
-        <button className='input__submit' type='submit' >Add</button>
+        <button className='input_submit' type='submit' >Add</button>
     </form>
   )
 }
