0545dbefec4be59860880ecf81879cdf2cbd4b5c
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-10-06 15:05:29 +0100

    todo logic

--- ts-react-todo-app/src/components/InputField.tsx
+++ ts-react-todo-app/src/components/InputField.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import './styles.css';
+import React from 'react'
+import './styles.css'
 
-const InputField = () => {
-  return (
-    <form className='input'>
-        <input type='input' placeholder='Add a task' className='input__box' />
-        <button className='input_submit' type='submit' >Add</button>
-    </form>
-  )
+interface Props {
+	todo: string
+	setTodo: React.Dispatch<React.SetStateAction<string>>
+    handleAdd: (e: React.FormEvent) => void
+}
+
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+
+    const inputRef = useRef(null)
+
+	return (
+
+
+		<form className='input' onSubmit={(e) => handleAdd(e)}>
+			<input type='input'
+            ref= {inputRef}
+            value={todo}
+            onChange={(e)=>setTodo(e.target.value)} 
+            placeholder='Add a task' 
+            className='input__box' />
+			<button className='input_submit' type='submit'>
+				Add
+			</button>
+		</form>
+	)
 }
 
 export default InputField
